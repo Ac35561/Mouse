c3578c11e69a80877ca8f59010555d6c8055978c
Ac35561 <ace.dev431@gmail.com>
2024-09-20 00:55:59 -0400

    Added server XP setup. View Issue #3 for more information.

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -50,6 +50,28 @@ module.exports = {
 						message.client.gExpCooldown.delete(message.author.id)
 					}, 10000)
 			}
+		});
+
+		// Handle server exp.
+		con.query(`SELECT * from server_experiance WHERE userid='${message.author.id}' AND serverid='${message.guild.id}'`, (err, rows) => {
+			if (err) {
+				throw err;
+			}
+
+			if (rows.length == 0) {
+				const newExp = generateExp();
+
+				con.query(`INSERT INTO server_experiance(userid, serverid, experiance) VALUES('${message.author.id}', '${message.guild.id}', '${newExp}')`);
+			} else {
+				const newExp = generateExp();
+				const currentExp = Number(rows[0].experiance);
+
+				const modifier = Number(rows[0].modifier);
+
+				const totalExp = Math.round((newExp * modifier) + currentExp);
+
+				con.query(`UPDATE server_experiance SET experiance='${totalExp}' WHERE userid='${message.author.id}' AND serverid='${message.guild.id}'`);
+			}
 		})
 	}
 }
